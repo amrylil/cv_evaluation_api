20e9f44ba6ebc22524f99884f41202c2759328cf
AmryLil <ulilamry432@gmail.com>
2025-10-01 18:54:44 +0800

    docs: update title swagger docs

--- src/openapi/index.ts
+++ src/openapi/index.ts
@@ -16,7 +16,7 @@ export function generateOpenApiSpec() {
   return generator.generateDocument({
     openapi: "3.0.0",
     info: {
-      title: "Digital Hero API",
+      title: "CV EVALUATIONS API",
       version: "1.0.0",
     },
     servers: [{ url: "http://localhost:3000/api/v1" }],
